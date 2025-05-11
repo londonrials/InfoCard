21bf36e18d532f997eba66086e1077c24456e5e5
haukeeem <malliekgr@gmail.com>
2025-05-10 21:27:45 -0500

    v1.0.1

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-// main.js
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript code here for interactivity and dynamic content
-    console.log('Welcome to my website!');
-});
